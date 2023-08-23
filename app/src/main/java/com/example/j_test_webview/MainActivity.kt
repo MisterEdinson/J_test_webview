144cf78ba9ea4764cb7de7a2099f9ca99c847e33
Edyard Karastoyka <88448179+MisterEdinson@users.noreply.github.com>
2023-08-23 12:17:13 +0500

    Internet availability check

--- app/src/main/java/com/example/j_test_webview/MainActivity.kt
+++ app/src/main/java/com/example/j_test_webview/MainActivity.kt
@@ -1,43 +1,45 @@
 package com.example.j_test_webview
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Context
+import android.net.ConnectivityManager
 import android.os.Bundle
 import android.util.Log
-import androidx.core.os.bundleOf
+import android.view.LayoutInflater
+import androidx.appcompat.app.AlertDialog
+import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.findNavController
 import com.android.volley.Request
 import com.android.volley.toolbox.StringRequest
 import com.android.volley.toolbox.Volley
 import com.example.j_test_webview.data.web.model.ResultUrlGet
 import com.example.j_test_webview.databinding.ActivityMainBinding
+import com.example.j_test_webview.databinding.DialogErrorBinding
 import com.example.j_test_webview.domain.util.Constants
 import com.google.gson.Gson
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
-    private lateinit var binding:ActivityMainBinding
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-        urlSet()
+        checkInternetConnection()
     }
 
-    private fun urlSet(){
+    private fun urlSet() {
         val queue = Volley.newRequestQueue(this)
         val url = Constants.BASE_URL
         val stringRequest = StringRequest(
             Request.Method.GET, url,
             { response ->
-                val res = Gson().fromJson(response,ResultUrlGet::class.java )
-                if(res.url != null){
-                    val bundle = Bundle()
-                    bundle.putString("url", res.url)
-                    findNavController(R.id.frContainer).navigate(R.id.webFragment, bundle)
-
-                }else{
-                    findNavController(R.id.frContainer).navigate(R.id.signFragment)
+                val res = Gson().fromJson(response, ResultUrlGet::class.java)
+                res.url?.let {
+                    appNav(R.id.signFragment)
+                    //appNav(R.id.webFragment, res.url)
+                } ?: run {
+                    appNav(R.id.signFragment)
                 }
             },
             { error ->
@@ -46,7 +48,38 @@ class MainActivity : AppCompatActivity() {
         queue.add(stringRequest)
     }
 
-    override fun onBackPressed() {
-        super.onBackPressed()
+    private fun appNav(id: Int, url: String) {
+        val bundle = Bundle()
+        bundle.putString("url", url)
+        findNavController(R.id.frContainer).navigate(id, bundle)
+    }
+
+    private fun appNav(id: Int) {
+        findNavController(R.id.frContainer).navigate(id)
+    }
+
+    private fun checkInternetConnection() {
+        val connectivityManager =
+            getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
+        val networkInfo = connectivityManager.activeNetworkInfo
+
+        if (networkInfo != null && networkInfo.isConnected) {
+            urlSet()
+        } else {
+            showNoInternetDialog()
+        }
+    }
+
+    private fun showNoInternetDialog() {
+        val builder = AlertDialog.Builder(this)
+        val dialogBind = DialogErrorBinding.inflate(LayoutInflater.from(this), null, false)
+        builder.setView(dialogBind.root)
+        val dialog = builder.create()
+
+        dialogBind.tvBtnOkDialogErr.setOnClickListener {
+            dialog.dismiss()
+            checkInternetConnection()
+        }
+        dialog.show()
     }
 }
--- app/src/main/java/com/example/j_test_webview/ui/sign/SignFragment.kt
+++ app/src/main/java/com/example/j_test_webview/ui/sign/SignFragment.kt
@@ -1,19 +1,27 @@
 package com.example.j_test_webview.ui.sign
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.example.j_test_webview.R
+import androidx.fragment.app.Fragment
+import com.example.j_test_webview.databinding.FragmentSignBinding
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class SignFragment : Fragment() {
+
+    private lateinit var binding: FragmentSignBinding
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        return inflater.inflate(R.layout.fragment_sign, container, false)
+    ): View {
+        binding = FragmentSignBinding.inflate(layoutInflater)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
     }
 }
